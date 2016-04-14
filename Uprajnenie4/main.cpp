4b246a4642942c264f4edaeaf9648f231db11209
Miroslav Vatev <MVatev@macbooks-macbook-air.local>
2016-04-14 20:09:12 +0300

    Initial Commit

---
+++ Uprajnenie4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Uprajnenie4
+//
+//  Created by Miroslav Vatev on 14/4/16.
+//  Copyright © 2016 Miroslav Vatev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
